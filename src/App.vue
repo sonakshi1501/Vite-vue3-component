df94211135af5a8d0bc8ae6abf025d0d81935c44
Sonakshi <sonakshishukla1501@gmail.com>
2022-02-14 16:06:04 +0530

    User update Api call

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Toolbar from './components/Toolbar.vue'
+import GithubUserInfo from './components/GithubUserInfo.vue'
+</script>
+
+<template>
+   <Toolbar/>
+  
+   <div class="container-fluid">
+     <div class="row">
+       <div class="col-md-12">
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores facere repellendus alias vero assumenda? Sunt distinctio quis alias culpa voluptatum nemo non nisi ad minus, dolor optio, fugit unde eius!</p>
+         <GithubUserInfo />
+       </div>
+     </div>
+   </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
---
+++ src/components/GithubUserInfo.vue
@@ -0,0 +1,45 @@
+<script >
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  mounted() {
+    this.getAllPosts();
+  },
+  methods: {
+    getAllPosts() {
+      fetch("https://api.github.com/users")
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          this.posts = data;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+};
+</script>
+
+<template>
+  <!-- {{posts}} -->
+  <div class="container m-0 p-0">
+    <div class="row">
+      <div class="col-md-4 mb-2" v-for="(post, index) in posts" :key="index">
+        <div class="card">
+          <img :src="post.avatar_url" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">{{ post.login }}</h5>
+            <a :href="post.html_url" class="btn btn-primary">Click Me</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
